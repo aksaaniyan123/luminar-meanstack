9d1898e85911957b27515d377974654e25779718
unknown <aksakaniyan96@gmail.com>
2021-03-30 16:33:29 +0530

    javascript is committed

---
+++ javascript/language fundamentals/languagefun.js
@@ -0,0 +1,7 @@
+//var age="25 years old";
+//var myname="hai i am varun";
+//console.log("hai iam "+age+" "+myname+"years old");
+
+var company="luminar";
+var location="kakkanad";
+console.log(`our company ${company} located in ${location}`);
