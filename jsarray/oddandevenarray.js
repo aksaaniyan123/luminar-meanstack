eaa6dfd62d7f504cd09e447f42a4c025c82425aa
unknown <aksakaniyan96@gmail.com>
2021-04-09 12:52:05 +0530

    jsarray committed

---
+++ jsarray/oddandevenarray.js
@@ -0,0 +1,13 @@
+var array = [10,3,4,6,8,5,7]
+var even = []
+var odd = []
+for(let num of array ){
+    if(num%2==0)
+    {
+        even.push(num)
+    }
+    else
+        {
+            odd.push(num);
+        }
+    }
