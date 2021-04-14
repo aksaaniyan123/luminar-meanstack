948a6f9a4f76ecf1b74a77245a9c9c0c312da42b
unknown <aksakaniyan96@gmail.com>
2021-04-14 13:44:05 +0530

    assignment is committed

---
+++ assignment/pattern.js
@@ -0,0 +1,10 @@
+var x,y,star;
+for(x=1; x <=6; x++)
+{
+   for (y=1; y < x; y++)
+     {
+    star=star+("*");        
+      }
+ console.log(star);
+ star=''; 
+}
---
+++ assignment/reverse.js
@@ -0,0 +1,6 @@
+var input="hello";//input string
+inputSplit=input.split("");//split the string
+inputReverse=inputSplit.reverse("");//reverse the string that is splitted o l l e h
+inputJoin=inputReverse.join("");//join the output
+console.log(inputJoin);
+
